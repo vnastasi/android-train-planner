9e0c390b6a489fe7557e9715c3474b34fdc8ff39
Valentin Nastasi <pragmatiku@protonmail.com>
2021-04-13 21:10:51 +0200

    Insert initial records in the test class itself

--- lib/persistence/src/androidTest/java/md/vnastasi/trainplanner/persistence/dao/FindBySearchQueryTest.kt
+++ lib/persistence/src/androidTest/java/md/vnastasi/trainplanner/persistence/dao/FindBySearchQueryTest.kt
@@ -10,7 +10,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.runBlocking
+import md.vnastasi.trainplanner.domain.station.StationType
+import md.vnastasi.trainplanner.persistence.client.impl.toStationEntity
+import md.vnastasi.trainplanner.persistence.data.createStation
 import md.vnastasi.trainplanner.persistence.util.DatabaseRule
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,6 +24,11 @@ internal class FindBySearchQueryTest {
 
     @get:Rule
     val databaseRule = DatabaseRule()
+
+    @Before
+    fun setUp() {
+        createRecords().forEach { runBlocking { databaseRule.stationDao.insert(it.toStationEntity()) } }
+    }
 
     @Test
     fun testEmptySearchQuery() = runBlocking {
@@ -62,4 +71,55 @@ internal class FindBySearchQueryTest {
     }
 
     private suspend inline fun <T> Flow<T>.expectOneElement(crossinline action: suspend (T) -> Unit) = take(1).collect(action)
+
+    private fun createRecords() = listOf(
+            createStation {
+                code = "0001"
+                shortName = "Den Bosch"
+                middleName = "'s-Hertogenbosch"
+                longName = "'s-Hertogenbosch"
+                synonyms = listOf("Hertogenbosch ('s)", "Den Bosch")
+                countryCode = "NL"
+                type = StationType.INTERCITY_JUNCTION_STATION
+                tracks = listOf("1", "3", "3a", "3b", "4", "4a", "4b", "6", "6a", "6b", "7", "7a", "7b")
+                latitude = 51.69048
+                longitude = 5.29362
+            },
+            createStation {
+                code = "0002"
+                shortName = "Amsterdam C"
+                middleName = "Amsterdam C."
+                longName = "Amsterdam Centraal"
+                synonyms = listOf("Amsterdam", "Amsterdam CS")
+                countryCode = "NL"
+                type = StationType.MAJOR_STATION
+                tracks = listOf("1", "2a", "2b", "4", "4a", "4b", "5", "5a", "5b", "7", "7a", "7b", "8", "8a", "8b", "10", "10a", "10b", "11", "11a", "11b", "13", "13a", "13b", "14", "14a", "14b", "15", "15a", "15b")
+                latitude = 52.3788871765137
+                longitude = 4.90027761459351
+            },
+            createStation {
+                code = "0003"
+                shortName = "Arnhem C"
+                middleName = "Arnhem C."
+                longName = "Arnhem Centraal"
+                synonyms = listOf("Arnhem", "Arnhem CS")
+                countryCode = "NL"
+                type = StationType.INTERCITY_JUNCTION_STATION
+                tracks = listOf("3", "4", "6", "6a", "6b", "7", "8", "9", "10", "11")
+                latitude = 51.9850006103516
+                longitude = 5.89916658401489
+            },
+            createStation {
+                code = "0004"
+                shortName = "De Vink"
+                middleName = "De Vink"
+                longName = "De Vink"
+                synonyms = emptyList()
+                countryCode = "NL"
+                type = StationType.STOP_TRAIN_STATION
+                tracks = listOf("1", "2", "3", "4")
+                latitude =  52.1472206115723
+                longitude = 4.4563889503479
+            }
+    )
 }
